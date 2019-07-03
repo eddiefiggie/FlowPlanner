99d3c9498e1d7df30a4cf40c336e91dc063f6400
eddiefiggie <eddiefiggie@protonmail.com>
2019-07-03 14:12:04 -0700

    addressed access issue with Transaction methods.
    Signed-off-by: eddiefiggie <eddiefiggie@protonmail.com>

--- src/main/java/com/flowplanner/application/flowplannerCLI.java
+++ src/main/java/com/flowplanner/application/flowplannerCLI.java
@@ -1,17 +1,22 @@
 package com.flowplanner.application;
 
 import com.flowplanner.persistence.Dao;
+import com.flowplanner.persistence.Transaction;
 import com.flowplanner.persistence.TransactionDao;
+
+import java.util.List;
 
 public class flowplannerCLI {
 
     public static void main(String[] args) {
         
-        Dao transactions = new TransactionDao();
+        Dao importedData = new TransactionDao();
+        List<Transaction> transactions = importedData.getAll();
 
         System.out.println("This is a test");
-        System.out.println(transactions.get(0).toString());
-        System.out.println(transactions.get(1).toString());
+        System.out.println(transactions.get(0).getDescription());
+        System.out.println(transactions.get(0).getAmount());
+        System.out.println(transactions.get(0).getDate());
 
     }
 }
